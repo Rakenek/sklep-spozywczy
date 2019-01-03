1e9ac5a330df9cee1f5a1aad6aca666f67e9f72b
Rakenek <kamilotawski@gmail.com>
2019-01-03 11:39:04 +0100

    moj projekt ustawienia

---
+++ sklepspozywczybackend/src/main/java/net/ko/sklepspozywczybackend/App.java
@@ -0,0 +1,13 @@
+package net.ko.sklepspozywczybackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
